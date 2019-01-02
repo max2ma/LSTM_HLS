8a7e02f378359a2f0673b78c349bcc886cff2f27
math@shaheen <liang.ma@studenti.polito.it>
2019-01-02 20:27:01 +0100

    update datatype and add time info

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 #include <cmath>
 #include "params.h"
@@ -8,18 +9,25 @@ void position(const DataType sensors[DataSize][Capa_In_Size+Infrared_In_Size], D
 
 int main(){
 
+	const int REP = 1000;
 	const	DataType sensors[DataSize][20]={
 #include "sensor_input.txt"
 	};
-	DataType pos[DataSize][2];
+	static DataType pos[REP][DataSize][2];
 	const DataType ref[DataSize][2]={
 #include "outputs.txt"
 	};
-	position(sensors,pos);
+	clock_t start = clock();
+	for(int i=0;i<REP;i++)
+	position(sensors,pos[i]);
+	clock_t t = clock() - start;
+	cout << "Execution completed"<<endl;
+	cout << "Execution time "<< (double)t /CLOCKS_PER_SEC <<" s"<<endl;
 	int err = 0;
+	for(int r=0;r<REP;r++)
 	for(int i=0;i<DataSize;i++)
 		for(int j=0;j<2;j++){
-			if(fabs(pos[i][j]/ref[i][j] -1) > 0.01){
+			if(fabs(pos[r][i][j]/ref[i][j] -1) > 0.01){
 				cout <<"pos["<<i<<"]["<<j<<"]="<<pos[i][j]<<'\t'
 					<<"ref["<<i<<"]["<<j<<"]="<<ref[i][j]
 					<<endl;

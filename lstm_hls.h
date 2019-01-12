420391fe293a6fec7e676e9c8973a3a6d806a7a3
math@shaheen <liang.ma@studenti.polito.it>
2019-01-12 13:23:33 +0100

    add resource limitation

--- lstm_hls.h
+++ lstm_hls.h
@@ -4,50 +4,33 @@
 template<typename T>
 const T sigmoid(const T value){
 	T v = expf(-value);
-//#pragma HLS RESOURCE variable=v core=Fexp_nodsp
+#pragma HLS RESOURCE variable=v core=Fexp_nodsp
 	T r = T(1)/(T(1) + v);
-//#pragma HLS RESOURCE variable=r core=FRecip_nodsp
-//#pragma HLS RESOURCE variable=r core=FAddSub_nodsp
+#pragma HLS RESOURCE variable=r core=FRecip_nodsp
+#pragma HLS RESOURCE variable=r core=FAddSub_nodsp
 	return r;
 }
 template<typename T>
 const T tanh(const T value){
 	T v2 = expf(2*value);
-//#pragma HLS RESOURCE variable=v2 core=Fexp_nodsp
+#pragma HLS RESOURCE variable=v2 core=Fexp_nodsp
 	T r = (v2-T(1))/(T(1) + v2);
-//#pragma HLS RESOURCE variable=r core=FRecip_nodsp
-//#pragma HLS RESOURCE variable=r core=FAddSub_nodsp
+#pragma HLS RESOURCE variable=r core=FRecip_nodsp
+#pragma HLS RESOURCE variable=r core=FAddSub_nodsp
 }
 
-template<int H, int W, typename T>
-void matMul_lstm(const T w[W][H<<2], const T v[W], T o[H<<2]){
-#pragma HLS ARRAY_PARTITION variable=w block factor=4 dim=2
-#pragma HLS ARRAY_PARTITION variable=o block factor=4 dim=1
-	for(int i=0;i<H;i++)
-#pragma HLS PIPELINE
-		for(int j=0;j<4;j++)
-			o[(i<<2)+j] = T(0);
-
-	for(int j=0;j<W;j++){
-		T r = v[j];
-		for(int i=0;i<H;i++)
-#pragma HLS PIPELINE
-			for(int k=0; k<4; k++)
-				o[(i<<2) + k]+=w[j][(i<<2)+k] * r;
-	}
-}
 #if 0
 template<int H, int W, int PE, typename T>
 void matMulAdd(const T w[W][H], const T v[W], const T bias[H], T o[H]){
 	T tmp_o[H];
-#pragma HLS ALLOCATION instances=fmul limit=PE operation
+//#pragma HLS ALLOCATION instances=fmul limit=PE operation
 	for(int i=0;i<H;i++)
 #pragma HLS PIPELINE
 		tmp_o[i] = bias[i];
 	for(int j=0;j<W;j++){
-#pragma HLS PIPELINE
 		T tmp_in = v[j];
 		for(int i=0;i<H;i++)
+#pragma HLS PIPELINE
 			tmp_o[i]+=w[j][i] * tmp_in;
 	}
 	for(int i=0;i<H;i++)
@@ -61,11 +44,12 @@ void matMulAdd(const T w[W][H], const T v[W], const T bias[H], T *o){
 #pragma HLS ARRAY_PARTITION variable=w complete dim=1
 #pragma HLS ARRAY_PARTITION variable=v complete dim=1
 	T tmp_o[H];
-#pragma HLS ALLOCATION instances=fmul limit=PE operation
+//#pragma HLS ALLOCATION instances=fmul limit=PE operation
 	for(int i=0;i<H;i++){
-#pragma HLS PIPELINE
+//#pragma HLS PIPELINE
 		T tmp_o = bias[i];
 		for(int j=0;j<W;j++)
+//#pragma HLS UNROLL
 			tmp_o+=w[j][i] * v[j];
 		o[i] = tmp_o;
 	}
@@ -95,16 +79,14 @@ void lstm(const T *input,
 		const int Samples){
 #pragma HLS ARRAY_PARTITION variable=BIAS block factor=2
 #pragma HLS ARRAY_PARTITION variable=WI block factor=2 dim=2
-//#pragma HLS ARRAY_PARTITION variable=WI complete dim=2
 #pragma HLS ARRAY_PARTITION variable=WS block factor=2 dim=2
-//#pragma HLS ARRAY_PARTITION variable=WS complete dim=2
 	static T state[LSTM_SIZE] = {0};
 	static T hidden[LSTM_SIZE] = {0};
 #pragma HLS ARRAY_PARTITION variable=hidden complete dim=1
 	T in[INPUT_SIZE];
 #pragma HLS ARRAY_PARTITION variable=in complete dim=1
 		T out[LSTM_SIZE];
-#pragma HLS ARRAY_PARTITION variable=out complete dim=1
+//#pragma HLS ARRAY_PARTITION variable=out complete dim=1
 	for(int rep=0; rep<Samples;rep++){
 
 		int start_point = rep * INPUT_SIZE;
@@ -113,6 +95,7 @@ void lstm(const T *input,
 			in[i]=input[start_point+i];
 		for(int i = 0; i < LSTM_SIZE; i++) {
 #pragma HLS PIPELINE II = 4
+//#pragma HLS ALLOCATION instances=fmul limit=40 operation
 			T g0 = BIAS[i];
 			T g1 = BIAS[1 * LSTM_SIZE + i];
 			T g2 = BIAS[2 * LSTM_SIZE + i];
@@ -130,7 +113,6 @@ void lstm(const T *input,
 				g3 +=WS[j][i + (LSTM_SIZE * 3)] * hidden[j];
 			}
 
-//#pragma HLS ALLOCATION instances=fexp limit=1 operation
 			T s = sigmoid(g1) * state[i] + sigmoid(g0) * tanh(g2);
 			state[i] = s;
 			out[i] = sigmoid(g3) * tanh(s);
@@ -151,7 +133,7 @@ void feed_forward(hls::stream<T> &in,
 		const int Samples){
 #pragma HLS INLINE off
 	for(int rep=0; rep<Samples; rep++){
-#pragma HLS DATAFLOW
+//#pragma HLS DATAFLOW
 		T input[INPUT_SIZE];
 		for(int i=0;i<INPUT_SIZE;i++)
 #pragma HLS PIPELINE

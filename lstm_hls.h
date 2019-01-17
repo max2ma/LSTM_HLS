97a89b28018c3fc05a34ad0a97edd5a5cc26c22d
math@shaheen <liang.ma@studenti.polito.it>
2019-01-17 17:16:04 +0100

    test passed

--- lstm_hls.h
+++ lstm_hls.h
@@ -4,57 +4,16 @@
 template<typename T>
 const T sigmoid(const T value){
 	T v = expf(-value);
-//#pragma HLS RESOURCE variable=v core=Fexp_nodsp
 	T r = T(1)/(T(1) + v);
-//#pragma HLS RESOURCE variable=r core=FRecip_nodsp
-//#pragma HLS RESOURCE variable=r core=FAddSub_nodsp
 	return r;
 }
 template<typename T>
 const T tanh(const T value){
 	T v2 = expf(2*value);
-//#pragma HLS RESOURCE variable=v2 core=Fexp_nodsp
 	T r = (v2-T(1))/(T(1) + v2);
-//#pragma HLS RESOURCE variable=r core=FRecip_nodsp
-//#pragma HLS RESOURCE variable=r core=FAddSub_nodsp
 }
 
-#if 0
-template<int H, int W, int PE, typename T>
-void matMulAdd(const T w[W][H], const T v[W], const T bias[H], T o[H]){
-	T tmp_o[H];
-//#pragma HLS ALLOCATION instances=fmul limit=PE operation
-	for(int i=0;i<H;i++)
-#pragma HLS PIPELINE
-		tmp_o[i] = bias[i];
-	for(int j=0;j<W;j++){
-		T tmp_in = v[j];
-		for(int i=0;i<H;i++)
-#pragma HLS PIPELINE
-			tmp_o[i]+=w[j][i] * tmp_in;
-	}
-	for(int i=0;i<H;i++)
-#pragma HLS PIPELINE
-		o[i] = tmp_o[i];
-}
-#else
 
-template<int H, int W, int PE, typename T>
-void matMulAdd(const T w[W][H], const T v[W], const T bias[H], T *o){
-#pragma HLS ARRAY_PARTITION variable=w complete dim=1
-#pragma HLS ARRAY_PARTITION variable=v complete dim=1
-	T tmp_o[H];
-//#pragma HLS ALLOCATION instances=fmul limit=PE operation
-	for(int i=0;i<H;i++){
-//#pragma HLS PIPELINE
-		T tmp_o = bias[i];
-		for(int j=0;j<W;j++)
-//#pragma HLS UNROLL
-			tmp_o+=w[j][i] * v[j];
-		o[i] = tmp_o;
-	}
-}
-#endif
 
 template<int N, typename T>
 void add(const T a0[N], const T a1[N], T a2[N]){
@@ -86,7 +45,6 @@ void lstm(const T *input,
 	T in[INPUT_SIZE];
 #pragma HLS ARRAY_PARTITION variable=in complete dim=1
 		T out[LSTM_SIZE];
-//#pragma HLS ARRAY_PARTITION variable=out complete dim=1
 	for(int rep=0; rep<Samples;rep++){
 
 		int start_point = rep * INPUT_SIZE;
@@ -95,7 +53,6 @@ void lstm(const T *input,
 			in[i]=input[start_point+i];
 		for(int i = 0; i < LSTM_SIZE; i++) {
 #pragma HLS PIPELINE II = 4
-//#pragma HLS ALLOCATION instances=fmul limit=40 operation
 			T g0 = BIAS[i];
 			T g1 = BIAS[1 * LSTM_SIZE + i];
 			T g2 = BIAS[2 * LSTM_SIZE + i];
@@ -123,7 +80,45 @@ void lstm(const T *input,
 			hidden[j] = out[j];
 	}
 }
+#if 1
+template<int H, int W, typename T>
+void matMulAdd(const T w[W][H], hls::stream<T> & v, const T bias[H], T o[H]){
+	T tmp_o[H];
+#pragma HLS ARRAY_PARTITION variable=tmp_o complete dim=1
+#pragma HLS ARRAY_PARTITION variable=bias complete dim=1
 
+	for(int i=0;i<H;i++)
+#pragma HLS UNROLL
+		tmp_o[i] = bias[i];
+
+	for(int j=0;j<W;j++){
+#pragma HLS PIPELINE
+		T tmp_in = v.read();
+		for(int i=0;i<H;i++)
+			tmp_o[i]+=w[j][i] * tmp_in;
+	}
+	for(int i=0;i<H;i++)
+#pragma HLS PIPELINE
+		o[i] = tmp_o[i];
+}
+#else
+
+template<int H, int W, typename T>
+void matMulAdd(const T w[W][H], const T v[W], const T bias[H], T *o){
+#pragma HLS ARRAY_PARTITION variable=w complete dim=1
+#pragma HLS ARRAY_PARTITION variable=v complete dim=1
+	T tmp_o[H];
+//#pragma HLS ALLOCATION instances=fmul limit=PE operation
+	for(int i=0;i<H;i++){
+//#pragma HLS PIPELINE
+		T tmp_o = bias[i];
+		for(int j=0;j<W;j++)
+//#pragma HLS UNROLL
+			tmp_o+=w[j][i] * v[j];
+		o[i] = tmp_o;
+	}
+}
+#endif
 
 template<int INPUT_SIZE, int OUTPUT_SIZE, typename T>
 void feed_forward(hls::stream<T> &in,
@@ -131,16 +126,8 @@ void feed_forward(hls::stream<T> &in,
 		const T W1[INPUT_SIZE][OUTPUT_SIZE],
 		const T BIAS1[OUTPUT_SIZE],
 		const int Samples){
-#pragma HLS INLINE off
 	for(int rep=0; rep<Samples; rep++){
-//#pragma HLS DATAFLOW
-		T input[INPUT_SIZE];
-		for(int i=0;i<INPUT_SIZE;i++)
-#pragma HLS PIPELINE
-			input[i] = in.read();
-//		T tmp[OUTPUT_SIZE];
-		matMulAdd<OUTPUT_SIZE, INPUT_SIZE, 4>(W1, input, BIAS1, output+ rep * OUTPUT_SIZE);
-//		add<OUTPUT_SIZE>(tmp, BIAS1, output[rep]);
+		matMulAdd<OUTPUT_SIZE, INPUT_SIZE>(W1, in, BIAS1, output+ rep * OUTPUT_SIZE);
 	}
 }
 

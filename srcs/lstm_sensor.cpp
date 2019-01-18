76b61daaa1fc83c2a69b11d1a440c73e4dd33de9
math@shaheen <liang.ma@studenti.polito.it>
2019-01-18 13:14:58 +0100

    update interface

---
+++ srcs/lstm_sensor.cpp
@@ -0,0 +1,36 @@
+#include "lstm_hls.h"
+#include "params.h"
+#include "hls_stream.h"
+
+void position(const DataType *input, DataType  *p, const int Samples){
+#pragma HLS INTERFACE m_axi depth=393*20 port=input bundle=gmem
+#pragma HLS INTERFACE m_axi depth=393*2 port=p bundle=gmem
+#pragma HLS INTERFACE s_axilite port=return bundle=control
+#pragma HLS INTERFACE s_axilite port=Samples bundle=control
+
+	//static const int InputSize = Capa_In_Size + Infrared_In_Size;
+	static const int LSTM_SIZE = 16;
+	
+	static const DataType WI[InputSize][LSTM_SIZE * 4] ={
+#include "expr_0.txt"
+	};
+	static const DataType WS[LSTM_SIZE][LSTM_SIZE * 4] = {
+#include "expr_1.txt"
+	};
+	static const DataType BiasS[LSTM_SIZE * 4]={
+#include "expr_2.txt"
+	};
+
+	static const DataType W1[LSTM_SIZE][Output_Size] ={
+#include "expr_3.txt"
+	};
+	static const DataType Bias1[Output_Size]={
+#include "expr_4.txt"
+	};
+
+#pragma HLS DATAFLOW
+	hls::stream<DataType> out_stream;
+#pragma HLS STREAM variable=out_stream depth=LSTM_SIZE*2 dim=1
+	lstm<InputSize, LSTM_SIZE>(input, out_stream, BiasS, WI, WS,Samples);
+	feed_forward<LSTM_SIZE, Output_Size>(out_stream, p, W1, Bias1,Samples);
+}
